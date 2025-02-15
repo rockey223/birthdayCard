96e84c65ab56beffeb0198a5d30f378fa66dbca5
rockey223 <rockeym50@gmail.com>
2025-02-15 15:22:45 +0545

    added gtm

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import Script from "next/script";
+import { GoogleTagManager } from "@next/third-parties/google";
 
 
 export const metadata: Metadata = {
@@ -17,39 +17,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <head>
-        {/* Meta Pixel Script */}
-        <Script
-          id="fb-pixel"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-              !function(f,b,e,v,n,t,s) {
-                if (f.fbq) return; n = f.fbq = function() {
-                  n.callMethod ? n.callMethod.apply(n, arguments) : n.queue.push(arguments)
-                };
-                if (!f._fbq) f._fbq = n; n.push = n; n.loaded = !0; n.version = '2.0';
-                n.queue = []; t = b.createElement(e); t.async = !0;
-                t.src = v; s = b.getElementsByTagName(e)[0];
-                s.parentNode.insertBefore(t, s)
-              }(window, document, 'script', 'https://connect.facebook.net/en_US/fbevents.js');
-
-              fbq('init', '1713854706197297');
-              fbq('track', 'PageView');
-            `,
-          }}
-        />
-
-        {/* Noscript Pixel Fallback */}
-        <noscript>
-          <img
-            height="1"
-            width="1"
-            style={{display:"none"}}
-            src="https://www.facebook.com/tr?id=1713854706197297&ev=PageView&noscript=1"
-          />
-        </noscript>
-      </head>
+      
       {/* <head>
         <link
           rel="icon"
@@ -66,6 +34,7 @@ export default function RootLayout({
         />
         <link rel="manifest" href="/site.webmanifest" />
       </head> */}
+      <GoogleTagManager gtmId="GTM-57ZL72TK"/>
       <body
         
       >
